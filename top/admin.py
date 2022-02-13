07357554a713cab42bf8bfee2bad3294f222d550
jang-han <jang-han@threet.co.jp>
2022-02-13 16:20:38 +0900

    20220213 models.py追加

--- top/admin.py
+++ top/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Dish
+from .models import Dish, Type, Course, Ingredient
 
 # Register your models here.
 admin.site.register(Dish)
+admin.site.register(Type)
+admin.site.register(Course)
+admin.site.register(Ingredient)
--- top/models.py
+++ top/models.py
@@ -1,17 +1,53 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    ingredient = models.CharField(max_length=20)
+    regist = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'id: ' + str(self.id) + ', ' + \
+               'ingredient: ' + str(self.ingredient) + ', ' + \
+               'regist: ' + str(self.regist) + ', ' + \
+               'update: ' + str(self.update)
+
+
+class Course(models.Model):
+    course = models.CharField(max_length=20)
+    regist = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'id: ' + str(self.id) + ', ' + \
+               'course: ' + str(self.course) + ', ' + \
+               'regist: ' + str(self.regist) + ', ' + \
+               'update: ' + str(self.update)
+
+               
+class Type(models.Model):
+    type = models.CharField(max_length=20)
+    regist = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'id: ' + str(self.id) + ', ' + \
+               'type: ' + str(self.type) + ', ' + \
+               'regist: ' + str(self.regist) + ', ' + \
+               'update: ' + str(self.update)
+
+        
 class Dish(models.Model):
     name = models.CharField(max_length=20)
-    ingredient1 = models.CharField(max_length=20)
-    ingredient2 = models.CharField(max_length=20)
-    ingredient3 = models.CharField(max_length=20)
-    ingredient4 = models.CharField(max_length=20)
-    ingredient5 = models.CharField(max_length=20)
-    course = models.IntegerField(default=0)
-    type = models.IntegerField(default=0)
-    regist = models.DateField()
-    update = models.DateField()
+    ingredient1 = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient1")
+    ingredient2 = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient2")
+    ingredient3 = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient3")
+    ingredient4 = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient4")
+    ingredient5 = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="ingredient5")
+    course = models.ForeignKey('Course', on_delete=models.CASCADE)
+    type = models.ForeignKey('Type', on_delete=models.CASCADE)
+    regist = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return 'id: ' + str(self.id) + ', ' + \
@@ -22,4 +58,9 @@ class Dish(models.Model):
                'ingredient4: ' + str(self.ingredient4) + ', ' + \
                'ingredient5: ' + str(self.ingredient5) + ', ' + \
                'course: ' + str(self.course) + ', ' + \
-               'type: ' + str(self.type)
+               'type: ' + str(self.type) + ', ' + \
+               'regist: ' + str(self.regist) + ', ' + \
+               'update: ' + str(self.update)
+    
+    class Meta:
+        ordering = ('update',)
