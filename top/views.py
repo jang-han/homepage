0da37787bc81816a29de06778a4a88f5093608e1
jang-han <jang-han@threet.co.jp>
2022-02-13 18:33:04 +0900

    20220213 料理PNG追加

--- top/views.py
+++ top/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Dish, Ingredient, Course, Type
 
 # Create your views here.
 def index(request):
@@ -9,8 +10,32 @@ def index(request):
     return render(request, 'top/index.html', params)
 
 def top(request):
+    # data = Dish.objects.all().values('ingredient1','ingredient2','ingredient3','ingredient4','ingredient5','course','type')
+    total_count = Dish.objects.all().count()
+    western_food = Dish.objects.filter(course=1)
+    japanese_food = Dish.objects.filter(course=2)
+    korean_food = Dish.objects.filter(course=3)
+    breakfast = Dish.objects.filter(type=1)
+    lunch = Dish.objects.filter(type=2)
+    dinner = Dish.objects.filter(type=3)
+    western_food_count = western_food.count()
+    japanese_food_count = japanese_food.count()
+    korean_food_count = korean_food.count()
+    breakfast_count = breakfast.count()
+    lunch_count = lunch.count()
+    dinner_count = dinner.count()
+    
+    
     params = {
         'page' : 'top/top.html',
+        'total_count' : total_count,
+        'western_food_count' : western_food_count,
+        'japanese_food_count' : japanese_food_count,
+        'korean_food_count' : korean_food_count,
+        'breakfast_count' : breakfast_count,
+        'lunch_count' : lunch_count,
+        'dinner_count' : dinner_count,
+        # 'data' : data,
     }
     return render(request, 'top/base.html', params)
 
